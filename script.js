b58a689cce12ae2161469ddca71a8a847d244530
John Kalumba <deejayjohnkay@gmail.com>
2026-06-19 11:53:38 -0700

    Add form and transaction elements to script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const form = document.getElementById("transaction-form");
+const description = document.getElementById("description");
+const amount = document.getElementById("amount");
+const list = document.getElementById("transaction-list");
+
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 function saveToStorage() {
