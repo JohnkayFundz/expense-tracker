58969d01147e33454031401db57c61e6bf96a726
John Kalumba <deejayjohnkay@gmail.com>
2026-06-19 11:18:58 -0700

    Simplify transaction updates and storage handling
    Refactor transaction handling to simplify updates.

--- script.js
+++ script.js
@@ -2,13 +2,4 @@ let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 function saveToStorage() {
   localStorage.setItem("transactions", JSON.stringify(transactions));
-}function updateStorageAndUI() {
-  saveToStorage();
-  updateUI();
-}transactions.push(newTransaction);
-updateStorageAndUI();transactions = transactions.filter((t) => t.id !== id);
-updateStorageAndUI();transactions = transactions.map((t) =>
-  t.id === editId ? { ...t, description: desc, amount: amt } : t
-);
-
-updateStorageAndUI();transactions.push / filter / mapupdateStorageAndUI()
+}
